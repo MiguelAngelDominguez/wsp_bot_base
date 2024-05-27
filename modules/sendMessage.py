d7d54e1725938766a925ae7017b3d4d8412434cb
pana-miguel <miguelangel.dominguez@unheval.pe>
2024-05-26 22:31:00 -0500

    Se agrego mas funciones a las clases

--- modules/sendMessage.py
+++ modules/sendMessage.py
@@ -1,32 +1,54 @@
 import requests
-import time
 
-TEST_MESSAGE = """Hola {name}, te saluda Unilix 🎉
+from datetime import datetime
+
+TEST_MESSAGE = """Hola, te saluda Unilix 🎉
 📢 Estamos enviando mensajes de prueva, para verificar los numeros vinculado
 📌 Esperamos no sea una molestia 😁."""
 
-TEST = """Hola {name}, te saludamos de la Academia TONER 📚✨
+TEST = """💥 PARTICIPA‼ GRATIS 🙂
+CONCURSO DE BECAS ✏📚
 
-Abrimos un ciclo SELECCION GENERAL
-🕣 HORARIO 🕣
-Lunes a viernes
-5:00 p.m - 8:00 p.m
+📣 Ciclo TODO EL DÍA ⏰☝
+Clases 💯 %PRESENCIALES
 
-Costo mensual {pat_money} soles
-Inicio de clases {date_start}
+💥 INSCRÍBETE AQUÍ: 👇
+▶ https://bit.ly/44zZ6QD ◀
 
-Informes e inscripciones
-{phones}
-{address}
-"""
+🗒 Jueves 23 mayo
+⏰ 4:30 p.m.
+
+📌 Lugar
+🏢 Jr. 28 de Julio N° 1098 - 4to piso | Frente al Banco de la Nación, a una cuadra de la Plaza de Armas. 
+
+🔥 No olvides llevar lápiz ✏, borrador y tú DNI.
+
+👉 ÚNETE A LA PRE MÁS GRANDE DE LA REGIÓN 👊 
+
+🎓 Tu mejor garantía:
+NUESTROS CACHIMBOS‼"""
 
 class msg:
     def __init__(self, para, mensaje, port):
         self.para = para
         self.mensaje = mensaje
         self.port = port
+    
+    def check_server(self):
+        url = f'http://localhost:{self.port}'
+        try:
+            response = requests.get(url)
+            return response.status_code == 200
+        except requests.exceptions.RequestException:
+            return False
 
     def sendMessage(self):
+        if not self.check_server():
+            with open('log_msgEnviados.txt', 'a') as log:
+                current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+                log.writelines(f"El servidor se cayó al enviar mensaje a {self.para} a las {current_time} por el puerto {self.port}\n")
+            return None
+        
         url = 'http://localhost:'+ str(self.port)
         data = {
             "message": self.mensaje,
@@ -35,12 +57,18 @@ class msg:
         headers = {
             'Content-Type': 'application/json'
         }
-        response = requests.post(url, json=data, headers=headers)
-        time.sleep(3)
-        return response
+        try:
+            response = requests.post(url, json=data, headers=headers)
+            response.raise_for_status()
+            return response
+        except requests.exceptions.RequestException as e:
+            with open('log_msgEnviados.txt', 'a') as log:
+                current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+                log.writelines(f"Error al enviar mensaje a {self.para} a las {current_time} por el puerto {self.port}: {str(e)}\n")
+            return None
 
 def main():
-    mensaje = msg('949152262', TEST, 3000)
+    mensaje = msg('949152262', TEST, 4000)
     mensaje.sendMessage()
 
 # main()
