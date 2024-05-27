1b256ac5195213478910f79716efce833adb28b8
pana-miguel <miguelangel.dominguez@unheval.pe>
2024-05-26 22:34:39 -0500

    Se modifico el funcion forInArrayNumber

--- main.py
+++ main.py
@@ -4,10 +4,11 @@ import modules.Table as tb
 import time
 import threading
 import os 
+from datetime import datetime
 
-TEST_PATH = './database/test.csv'
+TEST_PATH = './database/filtered_SMS.csv'
 
-NUMBER_SV = 1
+NUMBER_SV = 7
 
 MONEY_MSG = 800
 START_DATE = "12 de julio"
@@ -80,24 +81,35 @@ def divide_array(array, num_chips):
     return subarrays
 
 def forInArrayNumber(data, port):
-    for date in data:
-        # mensaje = send.msg(date[2], MESSAGE.format(
-        #     name = date[1], 
-        #     pay_money = MONEY_MSG, 
-        #     date_start = START_DATE,
-        #     phones = PHONES,
-        #     address = ADDRESS
-        #     ), port)
-        mensaje = send.msg(date[2], MSG_TORNER, port)
-        mensaje.sendMessage()
-        print(f"Se envio mensaje a {date[2]} - {date[1]} por el puerto {port}")
-        print("Delay de 15 segundos")
-        sleep(13)
+    total_numbers = len(data)
+    
+    for date in enumerate(data):
+        mensaje = send.msg(date[0], MSG_TORNER, port)
+        response = mensaje.sendMessage()
+        if response is None:
+            # El servidor está caído, parar el hilo.
+            current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+            with open('log_msgEnviados.txt', 'a') as log:
+                log.writelines(f"El servidor se cayó al enviar mensaje a {date[0]} a las {current_time} por el puerto {port}\n")
+                log.writelines(f"El hilo se detuvo en el número {date[0]} de {data[-1][0]}\n")
+            break
+        else:
+            current_time = datetime.now()
+            formatted_time = current_time.strftime('%Y-%m-%d %H:%M:%S')
+            with open('log_msgEnviados.txt', 'a') as log:
+                log.writelines(f"Se envió mensaje a {date[0]} a las {formatted_time} por el puerto {port}\n")
+            print(f"Se envió mensaje a {date[0]} por el puerto {port}")
+            print("Delay de 15 segundos")
+            sleep(15)
+    else:
+        with open('log_msgEnviados.txt', 'a') as log:
+            log.writelines(f"El hilo completó todos los envíos para el puerto {port}. Total números: {total_numbers}\n")
+
 
 def main():
     data_unilix_test = csv.csvReader(TEST_PATH)
     print(len(data_unilix_test.getData()))
-    data = data_unilix_test.getDatainRange()
+    data = data_unilix_test.getDatainRange(0, 5879)
     subarrays = divide_array(data, NUMBER_SV)
     i = 1
     for array in subarrays:
